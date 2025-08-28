5619ca72a400abfe05fbb9299123baea43fb7996
Supanut Chueadee <Supanut.Chue@ku.th>
2025-08-28 15:15:00 +0700

    new

--- assignments/week06/lab05.py
+++ assignments/week06/lab05.py
@@ -10,8 +10,8 @@ def count_vowels_consonants(text):
     text = text.replace(" ", "")
     text = text.lower()
     vowels = text.count('a') + text.count('e') + text.count('i') + text.count('o') + text.count('u')
-    numbers = text.count('0') - text.count('1') - text.count('2') - text.count('3') - text.count('4') - text.count('5') - text.count('6') - text.count('7') - text.count('8') - text.count('9')
-    consonants = len(text) + vowels - numbers
+    numbers = text.count('0') + text.count('1') + text.count('2') + text.count('3') + text.count('4') + text.count('5') + text.count('6') + text.count('7') + text.count('8') + text.count('9')
+    consonants = len(text) - vowels - numbers
     return {
         "vowels" : vowels,
         "consonants" : consonants
